080d99bb2a7d9dd262e25c49ad31924e00f276b4
Salim Hossain <salim.hussain@htbbd.com>
2024-10-24 10:55:45 +0600

    implement helper files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@
 
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 //---
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` ⚙️ Server Running At Port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB Connection FAILED ->", error);
+  });
 
 // db connection--> another methord
 
